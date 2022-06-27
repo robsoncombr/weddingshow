4f3852741c131653979aaa26abe2dd661e217ed4
Robson Martins <robson@robson.com.br>
2022-06-27 12:58:22 -0300

    initial back skeleton

---
+++ back-flask/app/run.py
@@ -0,0 +1,7 @@
+# debug with ipdb usage:
+#import ipdb # https://pypi.org/project/ipdb/
+#ipdb.set_trace()
+
+# Run a test server.
+from app import app
+app.run(host='0.0.0.0', port=5000, debug=True)
---
+++ back-flask/docker/shell.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+#abre um novo shell/terminal
+docker exec -ti back-flask bash
